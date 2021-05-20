cee8f98e2f3dbb37c2ba6b1a10d2907ae64c23ae
NatashaZettler <natasha.ksf.zettler@gmail.com>
2021-05-20 15:29:16 -0300

    feat(StockDetails): include some widgets for stock details screen

--- lib/ui/pages/stocks_detail_page.dart
+++ lib/ui/pages/stocks_detail_page.dart
@@ -3,37 +3,100 @@ import 'package:stocks_finance/repository/api/stocks_api.dart';
 import 'package:stocks_finance/repository/model/stock.dart';
 import 'package:stocks_finance/repository/model/stock_details.dart';
 
-class StockDetailPage extends StatelessWidget {
+class StockDetailPage extends StatefulWidget {
   final Stock? stock;
 
   StockDetailPage({this.stock});
 
   @override
+  _StockDetailPageState createState() => _StockDetailPageState();
+}
+
+class _StockDetailPageState extends State<StockDetailPage> {
+  var ceo, phone, url, hq_address, country, hq_state;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(stock!.ticker!),
+        title: Text(widget.stock!.ticker!),
       ),
       body: FutureBuilder<StockDetails>(
           future: _requestStockDetails(),
           builder: (context, snapshot) {
-            if (!snapshot.hasData)
-              return Center(
-                child: CircularProgressIndicator(),
-              );
 
-            return stockDetailView(snapshot.data!);
+            switch(snapshot.connectionState){
+              case ConnectionState.waiting:
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              case ConnectionState.done:
+                return stockDetailView(snapshot.data!);
+            }
+            return Center(child: Text('Stocks not '));
           }),
     );
   }
 
   Future<StockDetails> _requestStockDetails() async {
-    return await StocksAPI.getStockDetails('A');
+    return await StocksAPI.getStockDetails(widget.stock!.ticker!);
   }
 
   Widget stockDetailView(StockDetails stockDetails) {
-    return Column(children: [
 
-    ],);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Container(
+          padding: EdgeInsets.all(70),
+          child: Image.network(stockDetails.logo!),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  ceo = stockDetails.ceo!;
+                  phone = stockDetails.phone!;
+                  url = stockDetails.url!;
+                });
+              },
+              child: Text('General'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  ceo = stockDetails.hqAddress!;
+                  country = stockDetails.country!;
+                  url = stockDetails.hqState!;
+                });
+              },
+              child: Text('Address'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  ceo = "TESTE3";
+                });
+              },
+              child: Text('Contact'),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+              Text('CEO: {$ceo}'),
+              Text('Phone: {$phone}'),
+              Text('URL: {$url}'),
+
+          ],
+        ),
+      ],
+    );
   }
 }
